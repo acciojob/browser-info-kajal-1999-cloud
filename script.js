3c72ce16d014121b5da2c9cb948a31811518d3b7
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-10 16:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const div = document.createElement("div") ;
+div.idName = "browser-info" ;
+ div.innerText = `You are using ${navigator.appName} version${navigator.appVersion}`;
+document.body.append(div);
